584d9ee698bc2179fb3b9579e4f916c4417b84c0
Ojaswi <ojaswideep2020@gmail.com>
2025-07-14 14:44:07 +0530

    added header

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,15 @@
+import NavBar from "./components/NavBar";
+import { ThemeProvider } from "./components/theme-provider";
+
 function App() {
   return (
-    <></>
+    <>
+    <ThemeProvider>
+    <main className="w-screen min-h-screen bg-white dark:bg-zinc-900">
+      <NavBar />
+    </main>
+    </ThemeProvider>
+    </>
   )
 }
 
